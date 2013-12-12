60973e0cbe67cd36697bcae71a0378adca5241e7
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-12 23:25:30 +0000

    Update criar.h
    Parece que algures existe um nav por isso o nome foi alterado.

--- criar.h
+++ criar.h
@@ -3,6 +3,6 @@
 Navio criarNavio(int maxcontentores){
 	srand(time(NULL));
 	int random=(MAX_SCARGO*0.8+(rand()%(int)(MAX_SCARGO*0.2)+1));
-	Navio nav ={init_sem(random)), init_sem(MAX_SCARGO-random)}
-	Return nav;
+	Navio navio ={init_sem(random)), init_sem(MAX_SCARGO-random)}
+	Return navio;
 }
