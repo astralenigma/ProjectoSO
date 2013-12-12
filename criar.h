a6a00eed774c0388e2ef3c4349f6486fbab8d2d7
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-12 23:40:03 +0000

    Update criar.h
    Corrigiu-se os parametros, e acrescentou-se comentários

--- criar.h
+++ criar.h
@@ -1,6 +1,7 @@
 #include "cvs.h"
 
-Navio criarNavio(int maxcontentores){
+//Metodo que devolve um navio usar para criar o navio.
+Navio criarNavio(){
 	srand(time(NULL));
 	int random=(MAX_SCARGO*0.8+(rand()%(int)(MAX_SCARGO*0.2)+1));
 	Navio navio ={(init_sem(random)), (init_sem(MAX_SCARGO-random))};
