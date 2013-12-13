d9afd5b0d714b2e0f6cc8b69e83a2b5da704b57c
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-13 23:00:53 +0000

    Update transferencia.h
    Método para Descarregar contentores do navio trasladado.

--- transferencia.h
+++ transferencia.h
@@ -1,3 +1,20 @@
 #include "criar.h"
 
 //Inserir métodos para transferir os contentores
+void descarregarContentorDoNavio() {
+    alarm(30);
+
+    for (;;) {
+        sleep(5);
+        //Verificar os semáforos
+        P(emptyA);
+        P(mutexDescarga);
+        //estes apontadores não estão correctos
+	      n = *(p + out);
+        *(zonaCarga + inA) = n.contentoresCheios;
+        //Verificar os semáforos
+        V(mutexDescarga);
+        V(fullA);
+    }
+}
+
