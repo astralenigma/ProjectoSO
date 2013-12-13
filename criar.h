4e04e52737bf7d4eb339e0a06e09e83a7c68d8e3
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-13 23:20:47 +0000

    Update criar.h
    O método para inserir o navio tem de ser desenvolvido de forma a tornar o navio público.
    O método para testar o facto do navio ir embora usará um semáforo.

--- criar.h
+++ criar.h
@@ -1,11 +1,27 @@
 #include "cvs.h"
 
 //Metodo que devolve um navio usar para criar o navio.
-Navio criarNavio(){
+Navio criarNavio(void){
 	srand(time(NULL));
 	int random=(MAX_SCARGO*0.8+(rand()%(int)(MAX_SCARGO*0.2)+1));
 	Navio navio ={(init_sem(random)), (init_sem(MAX_SCARGO-random))};
 	return navio;
 }
 
+//Metodo para colocar o navio activo
+atracarNavio(){
+	srand(time(NULL));
+	for(;;){
+		//TODO Inserir código de criar navio
+		
+		//Código de espera para criar o novo navio
+		int random=(rand()%61);
+		sleep(300+random);
+		
+		//TODO Inserir teste de mandar o navio embora
+		
+		//TODO Inserir código de mandar o navio embora
+		
+	}
+}
 
