96a4348e72df37486217a34b812121f0f1e5eb07
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-15 23:27:45 +0000

    Update and rename criar.h to Navio.h
    Alterou-se o nome do ficheiro para ter mais a ver com as suas acções.
    Moveu-se o include para o main.c
    Alterou-se a criação do navio para ficar mais a haver com a estrutura

--- Navio.h
+++ Navio.h
@@ -1,10 +1,9 @@
-#include "cvs.h"
+
 
 //Metodo que devolve um navio usar para criar o navio.
 Navio criarNavio(void){
 	srand(time(NULL));
-	int random=(MAX_SCARGO*0.8+(rand()%(int)(MAX_SCARGO*0.2)+1));
-	Navio navio ={(init_sem(random)), (init_sem(MAX_SCARGO-random))};
+	Navio navio ={(MAX_SCARGO*0.8+(rand()%(int)(MAX_SCARGO*0.2)+1)), 0};
 	return navio;
 }
 
