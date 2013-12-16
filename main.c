3ad80a10416b81b50f611592110184fc8da4991b
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-16 15:07:15 +0000

    Update main.c
    Adição da criação de vários processos.

--- main.c
+++ main.c
@@ -5,9 +5,7 @@
 #include "transferencia.h"
 
 
-
-
-int main(void){
+main(){
 	int shmid;// id da memoria partilhada
 	char *addr;
 	int valorPid[MAX_CAMIOES], aguardaPid, aguardacliente, 		estadoCliente, child_stat;
@@ -15,6 +13,41 @@ int main(void){
 	mutex = init_sem(1);		/* exclusao mutua */
 	shmid = shmget(SHMKEY, MAX_CAMIOES, 0777|IPC_CREAT);
 	addr = (char*) shmat(shmid, 0, 0);
-	
-	return 0;
+	for (i = 0; i < MAX_CHILD; ++i) {
+		// printf("%d\n", i);
+		child_pid[i] = fork();
+		switch (child_pid[i]) {
+			case -1: /* error: no process created*/
+				perror("fork failed");
+				exit(1);
+                	break;
+			case 0: /*child process*/
+               
+				if (i == 0) {
+					printf("criarNavio => %d\n", i);
+					criarNavio();
+				}
+                		if (i == 1) {
+					printf("atracarNavio => %d\n", i);          
+                    			atracarNavio();
+                		}
+			break;
+			default: /*parent process*/
+				if (i == (MAX_CHILD - 1)) /* all childs created ?*/ { /* yes -> wait for termination	*/
+                    			for (j = 0; j < MAX_CHILD; ++j) {
+                        			wait_pid = wait(&child_stat);
+						if (wait_pid == -1) {
+	                            			perror("wait failed");
+						};
+                    			};
+					printf("All child processes have terminated.\n");
+					rel_sem(mutex);
+					rel_sem(empty);
+					rel_sem(full);
+					shmdt(addr);
+					shmctl(shmid, IPC_RMID, 0);
+                		};
+			break;
+		}; /*end switch*/
+	}; /*end for*/
 }
