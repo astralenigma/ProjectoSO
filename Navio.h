b034e0253ad6c4e2ef1d7ba42f91741d92a85299
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-16 18:46:35 +0000

    Update Navio.h
    Criou-se o controlo e corrigiu-se o criar navio e acrescentou-se uma variável de controlo.

--- Navio.h
+++ Navio.h
@@ -1,38 +1,33 @@
+semaphore semaNaviosAEspera = init_sem(0);
+
 //Metodo que devolve um navio usar para criar o navio.
 void criarNavio(void){
 	alarm(LIFETIME);
 	srand(time(NULL));
 	for(;;){
-		//TODO Inserir código de criar navio
-		
-		//Código de espera para criar o novo navio
 		int random=(rand()%61);
 		sleep(300+random);
-		V(nmrNaviosAEspera);
-		//TODO Inserir teste de mandar o navio embora
-		
-		//TODO Inserir código de mandar o navio embora
+		V(semaNaviosAEspera);
+		nmrNaviosAEspera++;
 	}	
-};
+}
 
 //Metodo para colocar o navio activo
 void atracarNavio(void){
 	srand(time(NULL));
-	P(nmrNaviosAEspera);
+	P(semaNaviosAEspera);
+	nmrNaviosAEspera--;
 	Navio navio ={(MAX_SCARGO*0.8+(rand()%(int)(MAX_SCARGO*0.2)+1)), 0};
 	*ship=navio;
-};
+}
 
 void controloNavio{
 	alarm(LIFETIME);
 	for(;;){
-		P(nmrNaviosAEspera);
 		atracarNavio();
-		while(!(nmrNaviosAEspera>0 && *ship.contentoreVazios>40)){
-			while(*ship.contentoresCheios!=0){
-				descarregarContentorNavio();
-			}
-			carregarContentorNavio();
-		}
+		printf("O navio nº %d atracou.",ship.id);
+		descarregarContentorNavio();
+		carregarContentorNavio();
+		printf("O navio nº %d partiu.",ship.id);
 	}
 }
