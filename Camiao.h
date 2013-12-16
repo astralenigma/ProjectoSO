5aa3fa4f539bf331eb98b4ab91448727d8401059
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-16 23:00:58 +0000

    Update Camiao.h
    WKWC.

--- Camiao.h
+++ Camiao.h
@@ -1,6 +1,7 @@
 Produto gerarInventario(){
   srand(time(NULL));
-  return { rand()%3,rand()%365, rand()%10};
+  Produto produto={ rand()%3,rand()%365, rand()%10};
+  return produto;
 }
 
 void carregarCamiao(){
