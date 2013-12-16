d19d9fd6df13c6c2d0bf410edcb910fe60718011
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-16 23:21:28 +0000

    Update Navio.h
    Está no ar, o batatoon.

--- Navio.h
+++ Navio.h
@@ -1,7 +1,7 @@
-
 //Metodo que devolve um navio usar para criar o navio.
 void criarNavio(void){
 	alarm(LIFETIME);
+	nmrNaviosCriados=0;
 	srand(time(NULL));
 	for(;;){
 		int random=(rand()%61);
@@ -16,7 +16,7 @@ void atracarNavio(void){
 	srand(time(NULL));
 	P(semaNaviosAEspera);
 	nmrNaviosAEspera--;
-	Navio navio ={(MAX_SCARGO*0.8+(rand()%(int)(MAX_SCARGO*0.2)+1)), 0};
+	Navio navio ={++nmrNaviosCriados,(MAX_SCARGO*0.8+(rand()%(int)(MAX_SCARGO*0.2)+1)), 0};
 	ship=&navio;
 }
 
