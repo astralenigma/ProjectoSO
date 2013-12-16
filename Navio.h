875b3edc8c9d593f2d726b4914c3477128cd4e8c
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-16 17:28:48 +0000

    Update Navio.h
    Criou-se os métodos de controlo e criação de navios.

--- Navio.h
+++ Navio.h
@@ -1,13 +1,6 @@
-
 //Metodo que devolve um navio usar para criar o navio.
-Navio criarNavio(void){
-	srand(time(NULL));
-	Navio navio ={(MAX_SCARGO*0.8+(rand()%(int)(MAX_SCARGO*0.2)+1)), 0};
-	return navio;
-};
-
-//Metodo para colocar o navio activo
-atracarNavio(){
+void criarNavio(void){
+	alarm(LIFETIME);
 	srand(time(NULL));
 	for(;;){
 		//TODO Inserir código de criar navio
@@ -15,11 +8,31 @@ atracarNavio(){
 		//Código de espera para criar o novo navio
 		int random=(rand()%61);
 		sleep(300+random);
-		
+		V(nmrNaviosAEspera);
 		//TODO Inserir teste de mandar o navio embora
 		
 		//TODO Inserir código de mandar o navio embora
-		
-	}
+	}	
 };
 
+//Metodo para colocar o navio activo
+void atracarNavio(void){
+	srand(time(NULL));
+	P(nmrNaviosAEspera);
+	Navio navio ={(MAX_SCARGO*0.8+(rand()%(int)(MAX_SCARGO*0.2)+1)), 0};
+	*ship=navio;
+};
+
+void controloNavio{
+	alarm(LIFETIME);
+	for(;;){
+		P(nmrNaviosAEspera);
+		atracarNavio();
+		while(!(nmrNaviosAEspera>0 && *ship.contentoreVazios>40)){
+			while(*ship.contentoresCheios!=0){
+				descarregarContentorNavio();
+			}
+			carregarContentorNavio();
+		}
+	}
+}
