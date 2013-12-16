9931c7daf9ac3a1abf8e73dca13e8237a83bd57b
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-16 22:16:43 +0000

    Update transferencia.h
    Primeira tentativa a resolver os erros.

--- transferencia.h
+++ transferencia.h
@@ -1,26 +1,24 @@
-semaphore mutexDescarga;
+semaphore mutexDescarga=init_sem(1);
 semaphore fullDescarga;
 semaphore emptyDescarga;
 
-semaphore mutexCarga;
+semaphore mutexCarga=init_sem(1);
 semaphore fullCarga;
 semaphore emptyCarga;
  
 
-//Inserir métodos para transferir os contentores
+
 void descarregarContentorNavio() {
 	int out = 0;
 	while(*ship.contentoresCheios !=0 ) {
 		//Decrementar o nº de contentores do navio no porto
-		//Alterar os semáforos
 		P(emptyDescarga);
 		P(mutexDescarga);
 		out++;
 		*ship.contentoresCheios--;
 		printf("O navio descarregou %d contentores",out);	
 		sleep(5);
-		//Aumentar o nº de contentores na zona de descarga (se calhar vai ser necessario usar semaforos)
-		//Alterar os semáforos
+		//Aumentar o nº de contentores na zona de descarga
 		V(fullDescarga);
 		V(mutexDescarga);
 		
@@ -30,13 +28,11 @@ void descarregarContentorNavio() {
 void carregarContentorNavio() {
 int in = 0;	
 	
-	while(!(*ship.contentoresVazios>=40&&nmrNaviosAEspera>0)){//O Ciclo tem de ser alterado
-		//Decrementar o nº de contentores na zona de carga (se calhar vai ser necessario usar semaforos)
-		//Alterar os semáforos
+	while(!(*ship.contentoresVazios>=40&&nmrNaviosAEspera>0)){
+		//Decrementar o nº de contentores na zona de carga
 		P(fullCarga);
 		P(mutexCarga);
 		//Incrementar o nº de contentores do navio no porto
-		//Alterar os semáforos
 		in++;
 		*ship.contentoresVazios++;
 		printf("Foi inserido %d contentores no navio", in);
@@ -52,10 +48,10 @@ int in = 0;
 void carregarContentorCamiao() {
 	//Diminuir o nº de contentores na zona de carga 
 	P(fullDescarga);
-	P(mutexDesCarga);
+	P(mutexDescarga);
 	sleep(5);
 	//Incrementar o nº de espaços na zona de carga
-	V(emptyDescarga)
+	V(emptyDescarga);
 	V(mutexDescarga);
 }
 
