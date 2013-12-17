bc05bf98d2a4d420f4d5d41aa961f51452179140
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-17 22:57:59 +0000

    Update simular.c
    Copiou-se a simulação do main para o método.

--- simular.c
+++ simular.c
@@ -1,2 +1,43 @@
 simular(){
+  for (i = 0; i < MAX_CHILD; i++) {
+                // printf("%d\n", i);
+                child_pid[i] = fork();
+                switch (child_pid[i]) {
+                        case -1: /* error: no process created*/
+                                perror("fork failed");
+                                exit(1);
+                        break;
+                        case 0: /*child process*/
+                                if (i == 0) {
+                                        criarNavio();
+                                }
+                                if (i == 1) {
+                                        controloNavio();
+                                }
+                                if (i >= 2) {
+                                        controloCamiao();
+                                }
+                        break;
+                        default: /*parent process*/
+                                if (i == (MAX_CHILD - 1)) /* all childs created ?*/ { /* yes -> wait for termination        */
+                                            for ( j = 0; j < MAX_CHILD; j++) {
+                                                wait_pid = wait(&child_stat);
+                                                if (wait_pid == -1) {
+                                                            perror("wait failed");
+                                                }
+                                            }
+                                        printf("All child processes have terminated.\n");
+                                        //rel_sem(mutex);
+                                        rel_sem(mutexDescarga);
+                                        rel_sem(fullDescarga);
+                                        rel_sem(emptyDescarga);
+                                        rel_sem(mutexCarga);
+                                        rel_sem(fullCarga);
+                                        rel_sem(emptyCarga);
+                                        shmdt(addr);
+                                        shmctl(shmid, IPC_RMID, 0);
+                                }
+                        break;
+                } /*end switch*/
+        }
 }
