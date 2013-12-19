792b9437b043315bb9ccfef781e398a1cedeea34
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-19 01:03:02 +0000

    Update Camiao.h
    Agora é que é.

--- Camiao.h
+++ Camiao.h
@@ -3,9 +3,9 @@ Produto produto;
 Produto gerarInventario(){
   srand(time(NULL));
   switch(rand()%3){
-    case 0: return (struct { frescos, 1, rand()%10});
-    case 1: return (struct { alimentar,rand()%365, rand()%10});
-    case 2: return (struct { npereciveis,0, rand()%10});
+    case 0: return (struct { TipoDeProduto frescos, 1, rand()%10});
+    case 1: return (struct { TipoDeProduto alimentar,rand()%365, rand()%10});
+    case 2: return (struct { TipoDeProduto npereciveis,0, rand()%10});
   }
   
 }
