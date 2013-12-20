67e26c5f449792b1f0cac5fac269e0f2d23e1a04
cutebutnasty <gredevil@hotmail.com>
2013-12-20 22:46:15 +0000

    Update cvs.h
    adicionou-se os defines das cores no cvs.h

--- cvs.h
+++ cvs.h
@@ -6,7 +6,14 @@
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) > (b) ? (a) : (b))
 #define SHMKEY (key_t)0x10
-
+//cores
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 //-------Variáveis alteradas pelo utilizador
 int maxCamioes;       /* max. # of truck processes        */
 int maxChilds;        /* max. # of child processes        */
