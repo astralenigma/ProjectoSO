fed37bff8ebe94416098508bc94bb66005ce1dbc
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-20 22:34:54 +0000

    Update Transferencia.h
    teste.

--- Transferencia.h
+++ Transferencia.h
@@ -2,9 +2,8 @@
 void descarregarContentorNavio() {
 	int out = 0;
 	while(ship.contentoresCheios !=0 ) {
-		fflush(stdout);
 		//Decrementar o nº de contentores do navio no porto
-		P(mutexDescarga);
+		//P(mutexDescarga);
 		P(emptyDescarga);
 		out++;
 		ship.contentoresCheios--;
@@ -14,7 +13,7 @@ void descarregarContentorNavio() {
 		printf("O navio descarregou %d contentores.\n",out);
 		//Aumentar o nº de contentores na zona de descarga
 		V(fullDescarga);
-		V(mutexCarga);
+		//V(mutexCarga);
 	}
 }
 //Método para carregar um contentor do Navio da zona de carga
@@ -23,8 +22,7 @@ int in = 0;
 	
 	while(!(ship.contentoresVazios>=40&& *apNmrNaviosAEspera>0)){
 		//Decrementar o nº de contentores na zona de carga
-		fflush(stdout);
-		P(mutexCarga);
+		//P(mutexCarga);
 		P(fullCarga);
 		//Incrementar o nº de contentores do navio no porto
 		in++;
@@ -34,7 +32,7 @@ int in = 0;
 		printf("O navio carregou %d contentores.\n",in);
 		++*nmrContentoresZC;
 		V(emptyCarga);
-		V(mutexCarga);
+	//	V(mutexCarga);
 		if(ship.contentoresVazios == 50){
 			break;
 		}	
@@ -43,8 +41,7 @@ int in = 0;
 //Método para carregar um contentor de um Camiao da zona de descarga
 void carregarContentorCamiao() {
 	//Diminuir o nº de contentores na zona de descarga
-	fflush(stdout);
-	P(mutexDescarga);
+//	P(mutexDescarga);
 	P(fullDescarga);
 	++*nmrContentoresZD;
 	printf("Um camião está a carregar um contentor.\n");
@@ -52,13 +49,12 @@ void carregarContentorCamiao() {
 	printf("Um camião foi carregado com um contentor.\n");
 	//Incrementar o nº de espaços na zona de descarga
 	V(emptyDescarga);
-	V(mutexCarga);
+//	V(mutexCarga);
 }
 //Método para descarregar um contentor de um Camiao para zona de carga
 void descarregarContentorCamiao() {
 	//Decrementar o nº de espaços na zona de carga
-	fflush(stdout);
-	P(mutexCarga);
+//	P(mutexCarga);
 	P(emptyCarga);
 	printf("Um camião está a descarregar um contentor.\n");
 	sleep(4);
@@ -66,5 +62,5 @@ void descarregarContentorCamiao() {
 	printf("Um Camião descarregou um contentor.\n");
 	//Aumentar o nº de contentores na zona de carga 
 	V(fullCarga);
-	V(mutexCarga);
+//	V(mutexCarga);
 }
