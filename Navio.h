d7f64655e2d1a8ed45315de0d94ba3cc4d3591e8
cutebutnasty <gredevil@hotmail.com>
2013-12-20 18:25:43 +0000

    Update Navio.h
    mudou-se a cor dos printf

--- Navio.h
+++ Navio.h
@@ -1,8 +1,16 @@
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 //Metodo que cria Navios
 void criarNavio(){
 	V(semaNaviosAEspera);
 	(*apNmrNaviosAEspera)++;
-	printf("Estão %d navios à espera.",*apNmrNaviosAEspera);
+	printf(ANSI_COLOR_GREEN"Estão %d navios à espera."ANSI_COLOR_RESET,*apNmrNaviosAEspera);
 }
 //Metodo que adiciona navios à lista de espera.
 void criarNavios(){
@@ -29,7 +37,7 @@ void controloNavio(){
 	alarm(LIFETIME);
 	for(;;){
 		atracarNavio();
-		printf("O navio nº %d atracou.\n",ship.id);
+		printf(ANSI_COLOR_GREEN"O navio nº %d atracou.\n"ANSI_COLOR_RESET,ship.id);
 		descarregarContentorNavio();
 		carregarContentorNavio();
 		printf("O navio nº %d partiu.\n",ship.id);
