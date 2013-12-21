20152350f16f8983b1b614857ea8f879faeecc6f
Rui Neves Carvalho <astralenigma@hotmail.com>
2013-12-21 00:59:42 +0000

    Update Navio.h
    Colocados cronometros no programa.

--- Navio.h
+++ Navio.h
@@ -1,10 +1,15 @@
 
-
 //Metodo que cria Navios
 void criarNavio(){
 	V(semaNaviosAEspera);
-	(*apNmrNaviosAEspera)++;
-	printf(ANSI_COLOR_GREEN"Estão %d navios à espera."ANSI_COLOR_RESET,*apNmrNaviosAEspera);
+	//V(semaCronometroInicio);
+	pide = fork();
+	if(pide==0){
+		cronometro();
+	}else{
+		(*apNmrNaviosAEspera)++;
+		printf(ANSI_COLOR_GREEN"Estão %d navios à espera."ANSI_COLOR_RESET,*apNmrNaviosAEspera);
+	}
 }
 //Metodo que adiciona navios à lista de espera.
 void criarNavios(){
@@ -20,6 +25,7 @@ void criarNavios(){
 //Metodo para colocar o navio activo
 void atracarNavio(){
 	srand(time(NULL));
+	V(semaCronometroFim);
 	P(semaNaviosAEspera);
 	(*apNmrNaviosAEspera)--;
 	Navio navio ={++(*barcosAtracados),(maxSCargo*0.8+(rand()%(int)(maxSCargo*0.2)+1)), 0};
